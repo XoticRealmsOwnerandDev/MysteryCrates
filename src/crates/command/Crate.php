060e5b293c784500169270a55e8fc0e369343e6f
PrimusLV <k.drivnieks@inbox.lv>
2015-12-24 09:45:25 +0200

    Update Crate.php

--- src/crates/command/Crate.php
+++ src/crates/command/Crate.php
@@ -8,6 +8,21 @@ use pocketmine\Player;
 
 class Crate extends Command implements PluginIdentifiableCommand {
 
-
+  /** @var MysteryCrates */
+  protected $main;
+  
+  public function __construct(MysteryCrates $plugin){
+    $this->main = $plugin;
+    
+    parent::__construct("crate", "MysteryCrate's main command", "/crates <?> [?]", []);
+  }
+  
+  public function execute(CommandSender $sender, $label, array $args){
+    
+  }
+  
+  public function getPlugin(){
+    return $this->main;
+  }
 
 }
