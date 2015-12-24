423b2f960ea314b88be1ae4ecbce8055fca9ad62
PrimusLV <k.drivnieks@inbox.lv>
2015-12-24 09:41:00 +0200

    Create Crate.php

---
+++ src/crates/command/Crate.php
@@ -0,0 +1,13 @@
+<?php
+namespace crates\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class Crate extends Command implements PluginIdentifiableCommand {
+
+
+
+}
